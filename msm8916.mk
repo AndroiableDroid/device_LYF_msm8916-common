67fc2be5fb23fbc90dd2b1896621f430d893e27c
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    msm8916-common: Migrate to audio V4 implementation
    * This puts an end to the goddamn logspam begging for 4.0 impl
    
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -27,10 +27,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@4.0-service \
     android.hardware.bluetooth.a2dp@1.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \

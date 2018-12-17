9b49c52e537aa84df381d02508a323d7f7186fab
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    msm8916-common: Uprev audio HAL to v5
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -31,10 +31,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@4.0-service \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \

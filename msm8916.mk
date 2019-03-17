2e4fff3763001ff05401b762ff5b40f4c38b518f
AndroiableDroid <androiabledroid@gmail.com>
2019-03-17 19:45:38 +0530

    msm8916-common: Add GPS to common
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -82,6 +82,15 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
+    gps.msm8916
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
+PRODUCT_PACKAGES += \
     libshims_get_process_name \
     libshims_flp \
     libshims_ims \

533b54c313ed8b29a43dfaff69fad85f83528b2c
Mohd Faraz <androiabledroid@gmail.com>
2019-03-17 05:14:57 +0000

    msm8916-common: Include Vendor
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -205,3 +205,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# inherit from the proprietary version
+$(call inherit-product, vendor/LYF/msm8916-common/msm8916-common-vendor.mk)

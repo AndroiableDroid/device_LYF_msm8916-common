c83d1a4f86d516bfab5470544062f034cb005895
AndroiableDroid <androiabledroid@gmail.com>
2019-03-22 02:02:54 +0530

    Revert "msm8916-common: no need this after update"
    This reverts commit 808201b719617ec54eddfbe28374c88a0008d1a0.
    
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,6 +164,13 @@ include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
 
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib64/libflp.so|libshims_flp.so \
+    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+    /system/vendor/lib/libflp.so|libshims_flp.so \
+    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211

efa9e61ad33c1d08261660902156090b92828e0b
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-05-14 10:43:34 +0000

    msm8916-common: vndk: Added More Libs
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -35,15 +35,18 @@ EXTRA_VENDOR_LIBRARIES := \
     libbinder \
     libui \
     android.hardware.configstore@1.0 \
+    android.hardware.configstore@1.1 \
     android.hardware.configstore-utils \
-    libsensor_vendor \
     libstagefright_foundation \
     libstdc++ \
     libnativehelper \
     libgui \
     android.hidl.token@1.0-utils \
+    android.hardware.graphics.allocator@2.0 \
+    android.hardware.graphics.composer@2.1 \
+    libfmq \
     android.hardware.graphics.bufferqueue@1.0 \
     android.hidl.token@1.0 \
     android.hardware.media.omx@1.0 \
     android.hardware.media@1.0 \
-    android.hardware.gatekeeper@1.0
+    vendor.qti.hardware.iop@1.0
